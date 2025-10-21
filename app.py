063f1e48a2df01e666b3ae511fcbc74b029191c8
Craig Fisher <140781290+craigfisher-dev@users.noreply.github.com>
2025-10-20 22:37:41 -0400

    Updated to newest data
    Going to begin process of storing in database

--- app.py
+++ app.py
@@ -33,14 +33,9 @@ response = supabase.table('Teams').select("*").execute()
 
 print(response)
 
-
-player_rankings = nfl.load_ff_rankings("week")
-
 current_season = nfl.get_current_season()
 
 current_week = nfl.get_current_week()
 
 print(f"It is the {current_season} season and its week {current_week}")
 
-# Outputs file to new CV file
-player_rankings.write_csv('player_rankings.csv')
